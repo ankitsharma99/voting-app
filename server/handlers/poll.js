2a1768d43def3e0ee5c4f0bb0bddc1645122becc
Ankit Sharma <sharmankit9593@gmail.com>
2022-03-16 01:09:40 +0530

    added singlePoll handler, vote handler and deletePoll handler + completed backend for vote app

--- server/handlers/poll.js
+++ server/handlers/poll.js
@@ -1,8 +1,9 @@
 const db = require("../models");
+const user = require("../models/user");
 
 exports.showPolls = async (req, res, next) => {
   try {
-    const polls = await db.Poll.find();
+    const polls = await db.Poll.find().populate("user", ["username", "id"]);
 
     res.status(200).json(polls);
   } catch (error) {
@@ -46,6 +47,85 @@ exports.createPoll = async (req, res, next) => {
     await user.save();
 
     res.status(201).json({ ...poll._doc, user: user._id });
+  } catch (error) {
+    error.status = 400;
+    next(error);
+  }
+};
+
+exports.getPoll = async (req, res, next) => {
+  try {
+    const { id } = req.params;
+
+    const poll = await db.Poll.findById(id).populate("user", [
+      "username",
+      "id",
+    ]);
+
+    if (!poll) {
+      throw new Error("No poll found");
+    }
+    res.status(200).json(poll);
+  } catch (error) {
+    error.status = 400;
+    next(error);
+  }
+};
+
+exports.deletePoll = async (req, res, next) => {
+  try {
+    const { id: pollId } = req.params;
+    const { id: userId } = req.decoded;
+
+    const poll = await db.Poll.findById(pollId);
+    if (!poll) {
+      throw new Error("No Poll found");
+    }
+    if (poll.user.toString() !== userId.toString()) {
+      throw new Error("Unauthorized access");
+    }
+    await poll.remove();
+    res.status(202).json(poll);
+  } catch (error) {
+    error.status = 400;
+    next(error);
+  }
+};
+
+exports.vote = async (req, res, next) => {
+  try {
+    const { id: pollId } = req.params;
+    const { id: userId } = req.decoded;
+
+    const { answer } = req.body;
+
+    if (answer) {
+      const poll = await db.Poll.findById(pollId);
+      if (!poll) {
+        throw new Error("No poll found");
+      }
+
+      const vote = poll.options.map((option) =>
+        option.option === answer
+          ? {
+              option: option.option,
+              _id: option._id,
+              votes: option.votes + 1,
+            }
+          : option
+      );
+
+      if (poll.voted.filter((user) => user.toString() === userId).length <= 0) {
+        poll.voted.push(userId);
+        poll.options = vote;
+        await poll.save();
+        res.status(202).json(poll);
+      } else {
+        throw new Error("Already voted");
+      }
+    } else {
+      throw new Error("No answer provided");
+    }
   } catch (error) {
     error.status = 400;
     next(error);
--- server/routes/poll.js
+++ server/routes/poll.js
@@ -7,4 +7,10 @@ router.route("/").get(handle.showPolls).post(auth, handle.createPoll);
 
 router.get("/user", auth, handle.usersPolls);
 
+router
+  .route("/:id")
+  .get(handle.getPoll)
+  .post(auth, handle.vote)
+  .delete(auth, handle.deletePoll);
+
 module.exports = router;
