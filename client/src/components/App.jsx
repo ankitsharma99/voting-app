635262b3bbd14ae89f6b2a9045f8396c23b23568
Ankit Sharma <sharmankit9593@gmail.com>
2022-03-23 17:56:07 +0530

    set up api service

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,7 +1,22 @@
-import React from "react";
+import React, {Component} from "react";
+import api from '../services/api';
 
-const App = () => {
-    return <div>App works</div>
-};
+// const App = () => {
+//     return <div>App works</div>
+// };
+
+class App extends Component {
+    async componentDidMount () {
+        const result = await api.call('post', 'auth/login', {
+            username: 'username',
+            password: 'password'
+        })
+        console.log(result);
+    }
+
+    render() {
+        return <div>App works</div>
+    }
+}
 
 export default App;
