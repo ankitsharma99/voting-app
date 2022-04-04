26c4f75c8c30893c063e864d2f0e3fd803ad45e0
Ankit Sharma <sharmankit9593@gmail.com>
2022-04-04 20:16:43 +0530

    added error messages

--- client/src/containers/App.jsx
+++ client/src/containers/App.jsx
@@ -1,15 +1,16 @@
 import React, { Component } from "react";
-import decode from 'jwt-decode';
+import decode from "jwt-decode";
 
-
+import Auth from "../components/Auth";
+import ErrorMessage from "../components/ErrorMessage";
 import { Provider } from "react-redux";
 import { setToken } from "../services/api";
-import {store} from '../store';
+import { store } from "../store";
 import { setCurrentUser } from "../store/actions/auth";
 import { addError } from "../store/actions/error";
 // import api from "../services/api";
 
-if(localStorage.jwtToken) {
+if (localStorage.jwtToken) {
   setToken(localStorage.jwtToken);
   try {
     store.dispatch(setCurrentUser(decode(localStorage.jwtToken)));
@@ -21,7 +22,10 @@ if(localStorage.jwtToken) {
 
 const App = () => (
   <Provider store={store}>
-    <div>App is working</div>
+    <div>
+      <Auth authType={"login"} />
+      <ErrorMessage />
+    </div>
   </Provider>
 );
 
