74961c265c9c5b1f99f861b731566367431e399e
Ankit Sharma <sharmankit9593@gmail.com>
2022-04-04 15:29:04 +0530

    finished redux setup

--- client/src/containers/App.jsx
+++ client/src/containers/App.jsx
@@ -1,9 +1,23 @@
 import React, { Component } from "react";
+import decode from 'jwt-decode';
+
+
 import { Provider } from "react-redux";
-
-import api from "../services/api";
-
+import { setToken } from "../services/api";
 import {store} from '../store';
+import { setCurrentUser } from "../store/actions/auth";
+import { addError } from "../store/actions/error";
+// import api from "../services/api";
+
+if(localStorage.jwtToken) {
+  setToken(localStorage.jwtToken);
+  try {
+    store.dispatch(setCurrentUser(decode(localStorage.jwtToken)));
+  } catch (err) {
+    store.dispatch(setCurrentUser({}));
+    store.dispatch(addError(err));
+  }
+}
 
 const App = () => (
   <Provider store={store}>
