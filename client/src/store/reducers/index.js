0168202a8614d0d6a9b9710d5ac1ec4e85ffa278
Ankit Sharma <sharmankit9593@gmail.com>
2022-04-06 14:50:03 +0530

    added polls actions and reducers

--- client/src/store/reducers/index.js
+++ client/src/store/reducers/index.js
@@ -1,8 +1,11 @@
 import { combineReducers } from "redux";
 import auth from "./auth";
 import error from "./error";
+import { polls, currentPoll } from "./polls";
 
 export default combineReducers({
   auth,
   error,
+  polls,
+  currentPoll,
 });
