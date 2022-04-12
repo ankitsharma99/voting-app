bafb00c31bb7d9e8635e8a75d4ea12dd3aa4c464
Ankit Sharma <sharmankit9593@gmail.com>
2022-04-12 13:37:25 +0530

    fixed minor bugs

--- client/src/containers/NavBar.jsx
+++ client/src/containers/NavBar.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { Fragment } from "react";
 import { Link } from "react-router-dom";
 import { connect } from "react-redux";
 
@@ -7,24 +7,45 @@ import { logout } from "../store/actions";
 const NavBar = ({ auth, logout }) => (
   <div className='navbar'>
     <div className='container'>
-      <ul className="navbar-container">
+      <ul className='navbar-container'>
         <li>
-          <Link className="navbar-brand" to='/'>Home</Link>
+          <Link className='navbar-brand' to='/'>
+            Home
+          </Link>
         </li>
-        <li>
-          <Link className="navbar-item" to='/register'>Register</Link>
-        </li>
-        <li>
-          <Link className="navbar-item" to='/login'>Login</Link>
-        </li>
-        <li>
-          <Link className="navbar-item" to='/poll/new'>Create Poll</Link>
-        </li>
-        <li>
-          <a className="navbar-item" onClick={logout}>Logout</a>
-        </li>
+        {!auth.isAuthenticated && (
+          <Fragment>
+            <li>
+              <Link className='navbar-item' to='/register'>
+                Register
+              </Link>
+            </li>
+            <li>
+              <Link className='navbar-item' to='/login'>
+                Login
+              </Link>
+            </li>
+          </Fragment>
+        )}
+        {auth.isAuthenticated && (
+          <Fragment>
+            <li>
+              <Link className='navbar-item' to='/poll/new'>
+                Create Poll
+              </Link>
+            </li>
+
+            <li>
+              <a className='navbar-item' onClick={logout}>
+                Logout
+              </a>
+            </li>
+          </Fragment>
+        )}
       </ul>
-      {auth.isAuthenticated && <p className="navbar-user">Logged in as {auth.user.username}</p>}
+      {auth.isAuthenticated && (
+        <p className='navbar-user'>Logged in as {auth.user.username}</p>
+      )}
     </div>
   </div>
 );
